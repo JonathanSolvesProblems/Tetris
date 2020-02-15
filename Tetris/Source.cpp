155e0750d1cfe58bfe4253cd8d7867954852908b
JonathanSolvesProblems <46333281+JonathanSolvesProblems@users.noreply.github.com>
2020-02-15 17:23:57 -0500

    added math with rotation of tetris blocks in a matrix, as well as the function rotate which does so based on the degrees of the blocks

---
+++ Tetris/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+wstring tetromino[7];
+
+int Rotate(int px, int py, int r);
+
+int main() {
+
+	// Assets
+	tetromino[0].append(L"..X...X...X...X.");
+	tetromino[1].append(L"..X..XX...X.....");
+	tetromino[2].append(L".....XX..XX.....");
+	tetromino[3].append(L"..X..XX..X......");
+	tetromino[4].append(L".X...XX...X.....");
+	tetromino[5].append(L".X...X...XX.....");
+	tetromino[6].append(L"..X...X..XX.....");
+
+
+	return 0;
+}
+
+int Rotate(int px, int py, int r)
+{
+	switch (r * 4)
+	{
+	case 0: return py * 4 + px;			// 0 degrees
+	case 1: return 12 + py - (px * 4);  // 90 degrees
+	case 2: return 15 - (py * 4) - px;  // 180 degrees
+	case 3: return 3 - py + (px * 4);   // 270 degrees
+	}
+	return 0;
+}
